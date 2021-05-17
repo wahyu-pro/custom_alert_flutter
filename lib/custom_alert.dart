271edf243f855e29fd97628d9940350e1a188153
wahyuwibowo <wahyuwibowo@mdd.co.id>
2021-05-17 11:19:24 +0700

    initial commit

---
+++ lib/custom_alert.dart
@@ -0,0 +1,37 @@
+library custom_alert;
+
+import 'package:flutter/material.dart';
+
+class CustomAlertBox {
+  /// Bu şekilde döküman yorumları oluşturabilirsiniz kullanan kişiler için faydalı olur.
+  static Future showCustomAlertBox({
+    @required BuildContext context,
+    @required Widget willDisplayWidget,
+  }) {
+    assert(context != null, "context is null!!");
+    assert(willDisplayWidget != null, "willDisplayWidget is null!!");
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
